06680186bdb2edc406c9a46f598445de1d0ccc8e
Syed Ali Hasnain Kazmi <sk22agc@herts.ac.uk>
2025-01-01 00:27:11 +0500

    Worker class is updated by adding Fee Calculation and Discount logic

--- src/Controller/Worker.java
+++ src/Controller/Worker.java
@@ -25,6 +25,17 @@ public class Worker {
             return "Parcel not found or already collected for customer: " + customer.getName();
         }
     }
+    
+    public double calculateFee(Parcel parcel) {
+        int baseFee = (parcel.getWeight() * 2) + (parcel.getDaysInDepot() * 2); // Each weight and days add 2 to the fees 
+        double volumeFee = parcel.getVolume() * 0.1; // Each unit of volume adds 0.1 to the fees
+        double totalFee = baseFee + volumeFee;
+
+        if (parcel.getParcelID().startsWith("C")) {
+            totalFee *= 0.8; // Apply 20% discount for parcels with 'C' prefix
+        }
+        return Math.round(totalFee); // Round off means if 20.6 is fees it would be 21.0 
+    }
 
     
 }
